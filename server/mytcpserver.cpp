db45eea68420dcb0f7279ce2960e08882106e960
Makar Konev <105596950+mertismk@users.noreply.github.com>
2023-03-30 12:54:39 +0300

    added multiplayer mode

---
+++ server/mytcpserver.cpp
@@ -0,0 +1,49 @@
+#include "mytcpserver.h"
+#include <QDebug>
+#include <QCoreApplication>
+
+MyTcpServer::~MyTcpServer()
+{
+    //mTcpSocket->close();
+    mTcpServer->close();
+    server_status=0;
+}
+MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
+    mTcpServer = new QTcpServer(this);
+    connect(mTcpServer, &QTcpServer::newConnection,
+            this, &MyTcpServer::slotNewConnection);
+
+    if(!mTcpServer->listen(QHostAddress::Any, 33333)){
+        qDebug() << "server is not started";
+    } else {
+        server_status=1;
+        qDebug() << "server is started";
+    }
+}
+
+void MyTcpServer::slotNewConnection(){
+    if(server_status==1){
+        QTcpSocket *cur_mTcpSocket;
+        cur_mTcpSocket = mTcpServer->nextPendingConnection();
+        cur_mTcpSocket->write("Hello, World!!! I am echo server!\r\n");
+        connect(cur_mTcpSocket, &QTcpSocket::readyRead,
+                this,&MyTcpServer::slotServerRead);
+        connect(cur_mTcpSocket,&QTcpSocket::disconnected,
+                this,&MyTcpServer::slotClientDisconnected);
+        mTcpSocket[cur_mTcpSocket->socketDescriptor()] = cur_mTcpSocket;
+    }
+}
+
+void MyTcpServer::slotServerRead(){
+    QTcpSocket *cur_mTcpSocket = mTcpSocket[mTcpServer->socketDescriptor()];
+    while(cur_mTcpSocket->bytesAvailable()>0)
+    {
+        QByteArray array =cur_mTcpSocket->readAll();
+        cur_mTcpSocket->write(array);
+    }
+}
+
+void MyTcpServer::slotClientDisconnected(){
+    QTcpSocket *cur_mTcpSocket = mTcpSocket[mTcpServer->socketDescriptor()];;
+    cur_mTcpSocket->close();
+}
